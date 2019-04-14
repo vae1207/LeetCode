9b3431906be36820bfe01ab8e334c6ff5d1cc225
vae1207 <43925525+vae1207@users.noreply.github.com>
2019-04-14 20:12:49 +0800

    Update 各位相加.py

--- 各位相加.py
+++ 各位相加.py
@@ -6,3 +6,15 @@ class Solution(object):
             if num == 0:
                 return 9
         return num
+
+    
+    方法二:
+ class Solution(object):
+    def addDigits(self, num):
+  
+        if num == 0:
+            return  0
+        if num%9 == 0:
+            return 9
+        else:
+            return num % 9
